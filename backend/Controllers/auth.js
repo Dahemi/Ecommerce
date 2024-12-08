1fcf186e1bc6cab492ae69060a9f0de76725f777
Dahami <126935880+Dahemi@users.noreply.github.com>
2024-12-08 18:03:19 +0530

    prt1

---
+++ backend/Controllers/auth.js
@@ -0,0 +1,36 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../Models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      //viewdProfile,
+      //impressions,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      //viewdProfile,
+      //impressions,
+    });
+  } catch (err) {}
+};
